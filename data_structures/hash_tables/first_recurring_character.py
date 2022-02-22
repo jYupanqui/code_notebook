5286218038c035880f1e9e90f108dc59e83756ac
Josue Yupanqui <jyupanqui@abartyshealth.com>
2022-02-22 14:23:43 -0400

    Added custom hash table.

---
+++ data_structures/hash_tables/first_recurring_character.py
@@ -0,0 +1,28 @@
+"""
+Given an array return the first recurring character
+Example:
+    ar = [2,5,1,2,3,5,1,2,4]:
+    returns 2
+
+    ar = [2,1,1,2,3,5,1,2,4]
+    returns 1
+
+    ar = [2,3,4,5]:
+    return None
+"""
+
+
+def first_recurring_character(nums):
+    bucket = {}
+    for i, n in enumerate(nums):
+        if n not in bucket:
+            bucket[n] = i
+        elif n in bucket:
+            return n
+        else:
+            return None
+
+
+nw = [2, 5, 1, 2, 3, 5, 1, 2, 4]
+x = first_recurring_character(nw)
+print(x)
--- data_structures/hash_tables/hash_tables.py
+++ data_structures/hash_tables/hash_tables.py
@@ -15,7 +15,74 @@ Hash Collisions:
 
 class HashTable:
     def __init__(self, size):
-        self.data = []
+        self.data = [None] * size
 
-    def _hash(self, key):
-        pass
+    def _my_hash(self, key):
+        """
+        ORD
+        Given a string representing one Unicode character, return an integer representing the Unicode code point of
+        that character. For example, ord('a') returns the integer 97 and ord('€') (Euro sign) returns 8364. T
+        his is the inverse of chr().
+        :param key:
+        :return:
+        """
+
+        h = 0
+        for i, k in enumerate(key):
+            h = (h + ord(k) * i) % len(self.data)
+        return h
+
+    def set(self, key, value):
+        address = self._my_hash(key)
+        if not self.data[address]:
+            self.data[address] = []
+        self.data[address].append([key, value])
+        print(self.data)
+
+    def get(self, key):
+        """
+        When there is low space it can change to o(n) due too hash collitions
+        :param key:
+        :return:
+        """
+        address = self._my_hash(key)
+        current_bucket = self.data[address]
+        if current_bucket:
+            for item in current_bucket:
+                if item[0] == key:
+                    return item[1]
+        else:
+            return None
+
+    def delete(self, key):
+        address = self._my_hash(key)
+        if self.data[address]:
+            for i, item in enumerate(self.data[address]):
+                if item[0] == key:
+                    self.data[address].pop(i)
+
+    def keys(self):
+        keys_list = []
+        for items in self.data:
+            if items:
+                for key in items:  # This is needed due to collitions
+                    keys_list.append(key[0])
+        return keys_list
+
+    def values(self):
+        val_list = []
+        for items in self.data:
+            if items:
+                for key in items:  # This is needed due to collitions
+                    val_list.append(key[1])
+        return val_list
+
+
+table = HashTable(2)
+table.set('grapes', 10)
+table.set('apples', 20)
+table.set('oranges', 23)
+table.delete('oranges')
+print(table.keys())
+print(table.values())
+# print(table.get('pepe'))
